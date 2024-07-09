8fdea601d040e82fbeb610ec6243f3748e609c3f
kishi1997 <topmo9634gmail.com>
2024-07-09 16:28:32 +0900

    Updated .gitignore

--- lib/utils/link_utils.dart
+++ lib/utils/link_utils.dart
@@ -1,9 +1,13 @@
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> launchURL(String url) async {
-  if (await canLaunch(url)) {
-    await launch(url);
-  } else {
-    throw 'Failed to launch $url';
+  try {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Cannot launch URL';
+    }
+  } catch (e) {
+    throw 'Failed to launch $url: $e';
   }
 }
