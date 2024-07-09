2536f9ec92ed1881b1aafd1cf4e8fd59f6e0da4b
kishi1997 <topmo9634gmail.com>
2024-07-09 14:12:48 +0900

    バリデーション処理の追加

--- lib/widgets/form/signup_form.dart
+++ lib/widgets/form/signup_form.dart
@@ -1,45 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:simple_sns_app/components/button/app_button.dart';
+import 'package:simple_sns_app/utils/validation_utils.dart';
 
-class SignupForm extends StatelessWidget {
+class SignupForm extends StatefulWidget {
   const SignupForm({super.key});
 
   @override
+  SignupFormState createState() {
+    return SignupFormState();
+  }
+}
+
+class SignupFormState extends State<SignupForm> {
+  final _formKey = GlobalKey<FormState>();
+  bool _isButtonEnabled = false;
+  void _validateForm() {
+    setState(() {
+      _isButtonEnabled = _formKey.currentState!.validate();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'アカウント名',
-          ),
-        ),
-        const SizedBox(height: 16),
-        const TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'メールアドレス',
-          ),
-        ),
-        const SizedBox(height: 16),
-        const TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'パスワード',
-          ),
-          obscureText: true,
-        ),
-        const SizedBox(height: 32),
-        AppButton(
-          text: 'はじめる',
-          onPressed: () {
-            // はじめるボタンの処理
-          },
-          backgroundColor: Theme.of(context).primaryColor,
-          textColor: Colors.white,
-        ),
-      ],
-    );
+    return Form(
+        key: _formKey,
+        onChanged: _validateForm,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'アカウント名',
+              ),
+              validator: (value) {
+                return CustomValidators.validateUsername(value);
+              },
+            ),
+            const SizedBox(height: 16),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'メールアドレス',
+              ),
+              validator: (value) {
+                return CustomValidators.validateEmail(value);
+              },
+            ),
+            const SizedBox(height: 16),
+            TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'パスワード',
+              ),
+              validator: (value) {
+                return CustomValidators.validatePassword(value);
+              },
+            ),
+            const SizedBox(height: 32),
+            AppButton(
+              text: 'はじめる',
+              onPressed: _isButtonEnabled
+                  ? () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Success')),
+                        );
+                      }
+                    }
+                  : null,
+              backgroundColor: _isButtonEnabled
+                  ? Theme.of(context).primaryColor
+                  : Colors.grey,
+              textColor: Colors.white,
+            ),
+          ],
+        ));
   }
 }
