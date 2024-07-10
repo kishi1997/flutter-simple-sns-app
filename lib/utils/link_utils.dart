9facefdff1c400911ee20ad98e780b281a909472
kishi1997 <topmo9634gmail.com>
2024-07-10 12:24:03 +0900

    Revert "Updated .gitignore"
    This reverts commit 8fdea601d040e82fbeb610ec6243f3748e609c3f.

--- lib/utils/link_utils.dart
+++ lib/utils/link_utils.dart
@@ -1,13 +1,9 @@
 import 'package:url_launcher/url_launcher.dart';
 
 Future<void> launchURL(String url) async {
-  try {
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Cannot launch URL';
-    }
-  } catch (e) {
-    throw 'Failed to launch $url: $e';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Failed to launch $url';
   }
 }
