29a640cabb4b9057a5693cb1f1df0651ea8956eb
kishi1997 <topmo9634gmail.com>
2024-07-18 11:21:28 +0900

    accessTokenKeyを環境変数ではなく定数として扱うように修正

--- lib/utils/token_utils.dart
+++ lib/utils/token_utils.dart
@@ -1,15 +1,16 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+const String accessTokenKey = "ACCESS_TOKEN_KEY";
 
 class TokenStorage {
   final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
   Future<void> saveToken(String token) async {
-    await _storage.write(key: dotenv.get('ACCESS_TOKEN_KEY'), value: token);
+    await _storage.write(key: accessTokenKey, value: token);
   }
 
   Future<String?> getToken() async {
-    return await _storage.read(key: dotenv.get('ACCESS_TOKEN_KEY'));
+    return await _storage.read(key: accessTokenKey);
   }
 }
 
