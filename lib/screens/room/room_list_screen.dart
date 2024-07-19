f87a675c60e882daf0df05f6234a31f4335d5114
kishi1997 <topmo9634gmail.com>
2024-07-20 02:12:38 +0900

    messageディレクトリを間違えて作ってしまっていたので修正

--- lib/screens/room/room_list_screen.dart
+++ lib/screens/room/room_list_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:simple_sns_app/components/header/app_header.dart';
 
-class MypageScreen extends StatelessWidget {
-  const MypageScreen({super.key});
+class RoomListScreen extends StatelessWidget {
+  const RoomListScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
