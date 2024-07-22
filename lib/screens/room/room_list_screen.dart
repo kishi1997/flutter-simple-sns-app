735e8f255dea8c56078c7546b1c16813b548113c
kishi1997 <topmo9634gmail.com>
2024-07-22 13:12:08 +0900

    PR分けるための修正

--- lib/screens/room/room_list_screen.dart
+++ lib/screens/room/room_list_screen.dart
@@ -1,72 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:simple_sns_app/components/header/app_header.dart';
-import 'package:simple_sns_app/domain/room/room_entity.dart';
-import 'package:simple_sns_app/domain/room/room_service.dart';
-import 'package:simple_sns_app/utils/logger_utils.dart';
-import 'package:simple_sns_app/widgets/room/room_tile.dart';
 
-class RoomListScreen extends StatefulWidget {
+class RoomListScreen extends StatelessWidget {
   const RoomListScreen({super.key});
   @override
-  RoomListScreenState createState() => RoomListScreenState();
-}
-
-class RoomListScreenState extends State<RoomListScreen> {
-  List<Room> _rooms = [];
-  bool _isLoading = false;
-
-  Future<void> getRooms() async {
-    setState(() {
-      _isLoading = true;
-    });
-    try {
-      await Future.delayed(const Duration(seconds: 3));
-      final rooms = await RoomService().getRooms();
-      setState(() {
-        _rooms = rooms;
-      });
-    } catch (e) {
-      logError(e);
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getRooms();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const AppHeader(title: 'room一覧ページ'),
-      body: Padding(
-        padding: const EdgeInsets.only(
-          top: 0,
-          right: 24.0,
-          bottom: 24.0,
-          left: 24.0,
-        ),
-        child: _isLoading
-            ? const Center(child: CircularProgressIndicator())
-            : _rooms.isEmpty
-                ? const Center(child: Text('チャットルームは存在しません'))
-                : Center(
-                    child: ListView.builder(
-                      itemCount: _rooms.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        final room = _rooms[index];
-                        return RoomTile(
-                          room: room,
-                        );
-                      },
-                    ),
-                  ),
-      ),
+    return const Scaffold(
+      appBar: AppHeader(title: 'room一覧ページ'),
     );
   }
 }
