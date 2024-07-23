bd0052212a12a2917e98074c5537fddfeda1b645
kishi1997 <topmo9634gmail.com>
2024-07-23 16:23:36 +0900

    ナビゲーションとメッセージ送信ボックスの実装

--- lib/screens/room/room_screen.dart
+++ lib/screens/room/room_screen.dart
@@ -4,6 +4,7 @@ import 'package:simple_sns_app/domain/message/message_entity.dart';
 import 'package:simple_sns_app/domain/message/message_service.dart';
 import 'package:simple_sns_app/utils/logger_utils.dart';
 import 'package:simple_sns_app/widgets/message/message_item.dart';
+import 'package:simple_sns_app/widgets/message/message_sender.dart';
 
 class RoomScreen extends StatefulWidget {
   final String roomId;
@@ -69,13 +70,14 @@ class RoomScreenState extends State<RoomScreen> {
         padding: const EdgeInsets.only(
           top: 0,
           right: 24.0,
-          bottom: 24.0,
+          bottom: 0,
           left: 24.0,
         ),
         child: Column(
           children: [
             Expanded(child: _buildListView()),
             const SizedBox(height: 24.0),
+            const MessageSender(),
           ],
         ),
       ),
---
+++ lib/widgets/message/message_sender.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:simple_sns_app/utils/validation_utils.dart';
+
+class MessageSender extends StatefulWidget {
+  const MessageSender({super.key});
+
+  @override
+  MessageSenderState createState() => MessageSenderState();
+}
+
+class MessageSenderState extends State<MessageSender> {
+  final TextEditingController _messageController = TextEditingController();
+  String? _messageErrorText;
+
+  @override
+  void initState() {
+    super.initState();
+    _messageController.addListener(() {
+      _validateField();
+    });
+  }
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  void _sendMessage() async {
+    // メッセージ送信のロジック
+  }
+
+  bool get _isFormValid {
+    return CustomValidators.validateEmpty(_messageController.text) == null;
+  }
+
+  void _validateField() {
+    setState(() {
+      _messageErrorText =
+          CustomValidators.validateEmpty(_messageController.text);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: TextField(
+            controller: _messageController,
+            style: const TextStyle(fontSize: 14),
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(8.0),
+              border: const OutlineInputBorder(),
+              hintText: 'メッセージを入力',
+              hintStyle: const TextStyle(color: Colors.grey),
+              errorText: _messageErrorText,
+            ),
+          ),
+        ),
+        const SizedBox(width: 12.0),
+        Container(
+          decoration: BoxDecoration(
+            color: _isFormValid ? Colors.lightGreen : Colors.grey[200],
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: IconButton(
+            icon: const Icon(Icons.send, color: Colors.white),
+            iconSize: 20,
+            onPressed: _isFormValid ? _sendMessage : null,
+          ),
+        ),
+      ],
+    );
+  }
+}
